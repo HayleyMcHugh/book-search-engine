a1ff87b3f3b90b1b8b185b8c922638d315f6252d
Hayley McHugh <hayleym522@gmail.com>
2022-03-13 22:11:42 -0700

    tried fixing port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
@@ -28,7 +28,7 @@ app.get('*', (req, res) => {
 });
 
 db.once('open', () => {
-  app.listen(PORT, () => {
+  app.listen(process.env.PORT || 3000, function(){
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
